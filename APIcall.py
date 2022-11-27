eaf6470c6f1b2e61159bde658178d23055dca4c8
BenNajiImed <98405005+BenNajiImed@users.noreply.github.com>
2022-11-27 18:41:06 +0100

    update

--- APIcall.py
+++ APIcall.py
@@ -9,7 +9,7 @@ parameters = {
 }
 headers = {
   'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': os.environ['API_KE'],
+  'X-CMC_PRO_API_KEY': os.environ['API_KEY'],
 }
 
 session = Session()
